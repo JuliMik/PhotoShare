efebefa1d2288dee861f9232d7852e34697494c0
JuliMik <gusachenkojulia@gmail.com>
2026-04-06 17:40:52 +0100

    feat: added db helper dispose to main app

--- src/photoshare/main.py
+++ src/photoshare/main.py
@@ -1,19 +1,31 @@
+
 import uvicorn
+
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
+from core.models import db_helper
 
 from api import router as api_router
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         port=settings.run.port,
         host=settings.run.host,
         reload=True,

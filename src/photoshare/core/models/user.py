24ffb0e0505eeef2544d99767ed06e1c8d96ea9c
JuliMik <gusachenkojulia@gmail.com>
2026-04-06 19:48:50 +0100

    feat: created user model

---
+++ src/photoshare/core/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, String, Text, text
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from .base import Base, timestamp_tz
+from src.photoshare.schemas.enums import UserRole
+
+
+class User(Base):
+    email: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(Text, nullable=False)
+    role: Mapped[str] = mapped_column(default=UserRole.USER)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, default=True, server_default=text("TRUE"), nullable=False
+    )

ddff8d03c47cbd3b16b645d35f1fcf0e63495f1a
JuliMik <gusachenkojulia@gmail.com>
2026-04-08 12:29:16 +0100

    feat: added naming convention

--- src/photoshare/core/models/base.py
+++ src/photoshare/core/models/base.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, MetaData
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import declared_attr
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from typing import Annotated
+from src.photoshare.core.config import settings
 
 
 from src.photoshare.utils import camel_case_to_snake_case
@@ -17,6 +18,10 @@ timestamp_tz = Annotated[
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:

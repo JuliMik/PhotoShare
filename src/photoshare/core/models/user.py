3b681fbac6d0bd837e328d58bbccca1b2556064d
JuliMik <gusachenkojulia@gmail.com>
2026-04-16 11:14:12 +0100

    feat: added mixin timestamps to user

--- src/photoshare/core/models/user.py
+++ src/photoshare/core/models/user.py
@@ -4,10 +4,12 @@ from sqlalchemy.orm import mapped_column
 
 from src.photoshare.core.models.base import Base
 from src.photoshare.schemas.enums import UserRole
+from .mixins import TimestampMixin
 
 
-class User(Base):
-    email: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
+class User(Base, TimestampMixin):
+    email: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(Text, nullable=False)
     role: Mapped[str] = mapped_column(default=UserRole.USER)
     is_active: Mapped[bool] = mapped_column(

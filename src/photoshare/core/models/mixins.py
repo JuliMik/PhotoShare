48d51a9567ac43ab87d9a46167891b2b22c44632
JuliMik <gusachenkojulia@gmail.com>
2026-05-19 12:25:17 +0100

    feat: fixed TimestampMixin

--- src/photoshare/core/models/mixins.py
+++ src/photoshare/core/models/mixins.py
@@ -4,5 +4,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )

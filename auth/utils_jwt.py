4febaef16557b55241b3c09d8a787a18bfbccab3
JuliMik <gusachenkojulia@gmail.com>
2026-05-19 12:29:02 +0100

    feat: added functions for validation and hash password

--- auth/utils_jwt.py
+++ auth/utils_jwt.py
@@ -1,21 +1,33 @@
 import bcrypt
 import jwt
-from core.config import settings
 from datetime import datetime, timedelta, timezone
+from src.photoshare.core.config import settings
+
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(), salt).decode("utf-8")
+
+
+def validate_password(password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password.encode("utf-8"),
+    )
 
 
 def encode_jwt(
     payload: dict,
     private_key: str = None,
     algorithm: str = settings.auth_jwt.algorithm,
-    expire_timedelta: timedelta | None = None,
     expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
 ):
     if private_key is None:
         private_key = settings.auth_jwt.private_key_path.read_text()
 
     to_encode = payload.copy()
-    now = datetime.now(timezone.utc).replace(tzinfo=None)
+    now = datetime.now(timezone.utc)
     if expire_timedelta:
         expire = now + expire_timedelta
     else:
@@ -24,43 +36,20 @@ def encode_jwt(
         exp=expire,
         iat=now,
     )
-    encoded = jwt.encode(
-        payload,
+    return jwt.encode(
+        to_encode,
         private_key,
         algorithm=algorithm,
     )
-    return encoded
 
 
 def decode_jwt(
     token: str | bytes,
-    public_key: str = None,
+    public_key: str = settings.auth_jwt.public_key_path.read_text(),
     algorithm: str = settings.auth_jwt.algorithm,
-):
-    if public_key is None:
-        public_key = settings.auth_jwt.public_key_path.read_text()
-
-    decoded = jwt.decode(
+) -> dict:
+    return jwt.decode(
         token,
         public_key,
         algorithms=[algorithm],
-    )
-    return decoded
-
-
-def hash_password(
-    password: str,
-) -> bytes:
-    salt = bcrypt.gensalt()
-    pwd_bytes: bytes = password.encode()
-    return bcrypt.hashpw(pwd_bytes, salt)
-
-
-def validate_password(
-    password: str,
-    hashed_password: bytes,
-) -> bool:
-    return bcrypt.checkpw(
-        password=password.encode(),
-        hashed_password=hashed_password,
     )
